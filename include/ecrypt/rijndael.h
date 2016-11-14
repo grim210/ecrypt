cf8c609f8c41d441af7501a50aeee13de73f385d
Joe Brown <brownj3199@live.com>
2016-11-14 00:03:30 -0600

    Fixed the include file to no longer use non-standard-width types, and fixed some spacing problems I had with the header.  Removed FULL_UNROLL code from the source file.

--- include/ecrypt/rijndael.h
+++ include/ecrypt/rijndael.h
@@ -31,28 +31,21 @@
 #include <stdint.h>
 #include "global.h"
 
-/* XXX: A placeholder for when I find-and-replace all instances. */
-typedef uint8_t		u_char;
-typedef uint8_t		u8;
-typedef uint16_t	u16;
-typedef uint32_t	u32;
-
 /*  The structure for key information */
 typedef struct {
-	int	enc_only;	/* context contains only encrypt schedule */
-	int	Nr;		/* key-length-dependent number of rounds */
-	u32	ek[4*(AES_MAXROUNDS + 1)];	/* encrypt key schedule */
-	u32	dk[4*(AES_MAXROUNDS + 1)];	/* decrypt key schedule */
+	int enc_only;	/* context contains only encrypt schedule */
+	int Nr;		/* key-length-dependent number of rounds */
+	uint32_t ek[4*(AES_MAXROUNDS + 1)];	/* encrypt key schedule */
+	uint32_t dk[4*(AES_MAXROUNDS + 1)];	/* decrypt key schedule */
 } rijndael_ctx;
 
-int	 rijndael_set_key(rijndael_ctx *, const u_char *, int);
-int	 rijndael_set_key_enc_only(rijndael_ctx *, const u_char *, int);
-void	 rijndael_decrypt(rijndael_ctx *, const u_char *, u_char *);
-void	 rijndael_encrypt(rijndael_ctx *, const u_char *, u_char *);
+int rijndael_set_key(rijndael_ctx *, const uint8_t *, int);
+int rijndael_set_key_enc_only(rijndael_ctx *, const uint8_t *, int);
+void rijndael_decrypt(rijndael_ctx *, const uint8_t *, uint8_t *);
+void rijndael_encrypt(rijndael_ctx *, const uint8_t *, uint8_t *);
 
-int	rijndaelKeySetupEnc(unsigned int [], const unsigned char [], int);
-int	rijndaelKeySetupDec(unsigned int [], const unsigned char [], int);
-void	rijndaelEncrypt(const unsigned int [], int, const unsigned char [],
-	    unsigned char []);
+int rijndaelKeySetupEnc(uint32_t *, const uint8_t *, int);
+int rijndaelKeySetupDec(uint32_t *, const uint8_t *, int);
+void rijndaelEncrypt(const uint32_t *, int, const uint8_t *, uint8_t *);
 
 #endif /* ECRYPT_RIJNDAEL_H */
