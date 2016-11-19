d620a1d42cc351a78955f11d9d8eb98cc7161990
Joe Brown <brownj3199@live.com>
2016-11-18 23:07:21 -0600

    Updated rijandael header file.

--- include/ecrypt/rijndael.h
+++ include/ecrypt/rijndael.h
@@ -12,6 +12,13 @@ typedef struct {
 	uint32_t dk[4*(AES_MAXROUNDS + 1)];	/* decrypt key schedule */
 } rijndael_ctx;
 
+struct rijndael_ctx_t {
+	int enc_only;
+	int Nr;
+	uint32_t ek[4*(AES_MAXROUNDS+1)];
+	uint32_t dk[4*(AES_MAXROUNDS+1)];
+};
+
 int rijndael_set_key(rijndael_ctx *, const uint8_t *, int);
 int rijndael_set_key_enc_only(rijndael_ctx *, const uint8_t *, int);
 void rijndael_decrypt(rijndael_ctx *, const uint8_t *, uint8_t *);
@@ -20,5 +27,16 @@ void rijndael_encrypt(rijndael_ctx *, const uint8_t *, uint8_t *);
 int rijndaelKeySetupEnc(uint32_t *, const uint8_t *, int);
 int rijndaelKeySetupDec(uint32_t *, const uint8_t *, int);
 void rijndaelEncrypt(const uint32_t *, int, const uint8_t *, uint8_t *);
+
+/* TODO:
+* Functions to match the blowfish code to make things more consistent.
+*/
+int rijndael_init(struct rijndael_ctx_t *ctx, const uint8_t *key,
+    uint32_t klen);
+int rijndael_release(struct rijndael_ctx_t *ctx);
+int rijndael_decrypt_ctr(struct rijndael_ctx_t *ctx, uint64_t iv,
+    const uint8_t *ct, uint32_t ct_len, uint8_t* out);
+int rijndael_encrypt_ctr(struct rijndael_ctx_t *ctx, uint64_t iv,
+    const uint8_t* pt, uint32_t pt_len, uint8_t* out);
 
 #endif /* ECRYPT_RIJNDAEL_H */
